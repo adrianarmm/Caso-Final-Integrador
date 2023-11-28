9c3271c46b871283a3389a5f4474e0af95a5d007
adrianarmm <areyemor@myuax.com>
2023-11-28 12:19:04 +0100

    8

--- tiny.lisp.cpp
+++ tiny.lisp.cpp
@@ -13,3 +13,9 @@ public:
     using proc_type = Variant(*)(const std::vector<Variant>&);
     using iter = std::vector<Variant>::const_iterator;
     using map = std::map<std::string, Variant>;
+
+    variant_type type;
+    std::string val;
+    std::vector<Variant> list;
+    proc_type proc;
+    Entorno* env;
