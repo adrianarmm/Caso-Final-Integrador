91c1e4f4c997bf8775f092dd077c804bde431db1
adrianarmm <areyemor@myuax.com>
2023-11-28 12:18:35 +0100

    7

--- tiny.lisp.cpp
+++ tiny.lisp.cpp
@@ -3,3 +3,13 @@
 #include <map>
 #include "json11.hpp"
 
+enum variant_type { Symbol, Number, List, Proc, Lambda, Cadena };
+
+struct Entorno;
+
+class Variant
+        {
+public:
+    using proc_type = Variant(*)(const std::vector<Variant>&);
+    using iter = std::vector<Variant>::const_iterator;
+    using map = std::map<std::string, Variant>;
